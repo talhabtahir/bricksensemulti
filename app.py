472395f8f4878776792f3ca487efb39c61ade6ca
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-04 16:01:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,42 @@ PAGES = {
 }
 
 def main():
-    # Set the page configuration
     st.set_page_config(
         page_title="Brick Detection App",
-        page_icon="static/brickicon3.png",
+        page_icon="static/brickicon8.png",
         layout="centered"
     )
+    
+    # Authentication (example: simple password protection)
+    if 'authenticated' not in st.session_state:
+        st.session_state.authenticated = False
 
+    if not st.session_state.authenticated:
+        password = st.text_input("Enter the password to access the app", type="5488")
+        if password == "your_password_here":
+            st.session_state.authenticated = True
+            st.experimental_rerun()  # Rerun the app to update session state
+        else:
+            st.error("Incorrect password. Please try again.")
+            return
+
+    # Navigation
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", list(PAGES.keys()))
 
-    # Page access control
+    # Load the appropriate page
     if page == "Page 1":
         if "access_page_1" in st.session_state and st.session_state.access_page_1:
-            # Import and run Page 1 code
             import page1
         else:
             st.warning("You do not have access to this page.")
     elif page == "Page 2":
         if "access_page_2" in st.session_state and st.session_state.access_page_2:
-            # Import and run Page 2 code
             import page2
         else:
             st.warning("You do not have access to this page.")
     elif page == "Page 3":
         if "access_page_3" in st.session_state and st.session_state.access_page_3:
-            # Import and run Page 3 code
             import page3
         else:
             st.warning("You do not have access to this page.")
